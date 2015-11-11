e662a4e08d168aca942c7be96a339b4b52573d7d
Falko Richter <falko@sensorberg.com>
2015-11-11 17:06:14 +0100

    Revert "fixed the update script"
    This reverts commit 1bc9a5f386025b94795ca1cfc98059f1c4d56695.

--- resolver/src/main/resources/scripts/layoutLogReportedBackHistory.groovy
+++ resolver/src/main/resources/scripts/layoutLogReportedBackHistory.groovy
@@ -1,9 +1,5 @@
 if(ctx._source.reportedBackHistory == null) {
-    if (ctx._source.reportedBack != null) {
-        ctx._source.reportedBackHistory = [ ctx._source.reportedBack ]
-    } else {
-        ctx._source.reportedBackHistory = []
-    }
+    ctx._source.reportedBackHistory = [ctx._source.reportedBack];
 }
-ctx._source.reportedBackHistory += currentStatus
-ctx._source.reportedBack = currentStatus
+ctx._source.reportedBackHistory += currentStatus;
+ctx._source.reportedBack = currentStatus;                                      
